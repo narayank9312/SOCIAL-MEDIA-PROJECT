33cf459be4d6a908cc56b554e75ae0700e88d91c
Narayan Kumar Jha <narayank9312@gmail.com>
2021-02-26 17:40:38 +0530

    post controller made
    Signed-off-by: Narayan Kumar Jha <narayank9312@gmail.com>

--- README.md
+++ README.md
@@ -26,3 +26,38 @@ D:\SOCIAL-MEDIA-PROJECT\SRC
 └───routes                    # express middleware (route wose)
 
 ```
+
+## business logic
+
+### users
+
+1. **create users**
+   this will create a new user with a random username
+
+### posts
+
+1 **create posts**
+this will create a new post, required fieldare
+-username (the author of this posts)
+-title
+-body
+
+2.  **show all posts**
+
+    list all existing posts, we should have
+    following filtering support
+
+    -filter by username
+    -filter by query contained in title (search by title)
+
+3.  **edit posts** `tbd`
+
+4> **delete posts** `tbd`
+
+### comments
+
+1. **show all comments (of a user)**
+
+2 **show all comments (under a posts)**
+
+3 **add a comments**
---
+++ src/controllers/posts.js
@@ -0,0 +1,56 @@
+const {Posts, Users} = require('../db/models');
+
+async function createNewPosts(userId, title, body) {
+  const post = await Posts.create({
+    title,
+    body,
+    userId,
+  });
+  return post;
+}
+
+/**
+ * showAllPosts({username: ''})
+ * showAllPosts({title: ''})
+ */
+async function showAllPosts(query) {
+  //TODO : handle querry params
+  const posts = await Posts.findAll({include: [Users]});
+
+  return posts;
+}
+
+module.exports = {
+  createNewPosts,
+  showAllPosts,
+};
+
+/**
+ * test code
+ */
+
+/*
+
+async function task() {
+  //   console.log(
+  //     await createNewPosts(
+  //       1,
+  //       'this is a sample post',
+  //       'body of the post goes here'
+  //     )
+  //   ),
+  //     console.log(
+  //       await createNewPosts(
+  //         2,
+  //         'Another is a sample post',
+  //         'same body example here as well '
+  //       )
+  //     );
+
+  const posts = await showAllPosts();
+  for (let p of posts) {
+    console.log(`${p.title}\nauthor: ${p.user.username}\n${p.body}\n=======\n`);
+  }
+}
+task();
+*/
