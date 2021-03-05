455fa13b9775fda9ac578fe64a3b609abbf45bee
Narayan Kumar Jha <narayank9312@gmail.com>
2021-03-05 16:41:41 +0530

    get all posts and show at home
    Signed-off-by: Narayan Kumar Jha <narayank9312@gmail.com>

--- src/public/app/cbsocial-common.js
+++ src/public/app/cbsocial-common.js
@@ -1,6 +1,7 @@
 $(() => {
   $('#navbar').load('component/navbar.html', loginIfNeed);
   $('#footer').load('component/footer.html');
+  $('#content').load('component/all-posts.html', loadPosts);
 
   //   loginIfNeed();
 });
@@ -30,4 +31,36 @@ function loginIfNeed() {
     console.log($('#nav-username'));
     $('#nav-username').text(currentUser.username);
   }
+}
+
+function loadPosts() {
+  $.get('/api/posts', (posts) => {
+    for (let p of posts) {
+      //   console.log(p);
+      $('#posts-container').append(
+        $(`
+        <div class="col-4">
+        
+        <div class="card m-2" >
+        <div class="card-body">
+          <h5 class="card-title">${p.title}</h5>
+          <h6 class="card-subtitle mb-2 text-muted">${p.user.username}</h6>
+          <p class="card-text">
+            ${p.body.substr(0, 200)}
+             ...<a href="#">...read more..></a>
+          </p>
+          <a href="#" class="card-link">Comments</a>
+          <a href="#" class="card-link">Like</a>
+        </div>
+      </div>
+
+
+        </div>
+        
+      
+      
+        `)
+      );
+    }
+  });
 }
